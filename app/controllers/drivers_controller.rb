898f35814a04667b3fccce3a574af4975ccf0ba9
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-07 15:26:53 -0700

    added driver index page and created a table!

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,17 +1,7 @@
 class DriversController < ApplicationController
   
-  # DRIVERS = [
-  #   { name: 'Becca',
-  #     vin: 12345
-  #   },
-  #   { name: 'Sharon',
-  #     vin: 45678
-  #   }
-  # ]
-
-  
   def index
-    @drivers = 
+    @drivers = Driver.all
   end
 
   def show
