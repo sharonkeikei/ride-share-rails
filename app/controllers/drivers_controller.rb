74f6e5d7a945cd05dca974ef1d025805c05406ff
Sharon Cheung <keikei1128@g.ucla.edu>
2020-05-07 14:56:34 -0700

    generated controller & model & seed db

---
+++ app/controllers/drivers_controller.rb
@@ -0,0 +1,20 @@
+class DriversController < ApplicationController
+  
+  # DRIVERS = [
+  #   { name: 'Becca',
+  #     vin: 12345
+  #   },
+  #   { name: 'Sharon',
+  #     vin: 45678
+  #   }
+  # ]
+
+  
+  def index
+    @drivers = 
+  end
+
+  def show
+
+  end
+end
