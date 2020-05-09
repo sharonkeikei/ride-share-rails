27bc3ee508622ce760d94a7a1be786fc60af9a50
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-09 16:55:57 -0700

    Added trips to routes.rb, created RESTful routes in trip_controller, and added views for show and edit.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,80 @@
 class TripsController < ApplicationController
+
+  # def index
+  #   @trips = Trips.order(:id).all
+  # end
+
+  def show
+    id = params[:id]
+    @trip = Trip.find_by(id: id)
+
+    # redirect to passenger OR driver show page???
+    if @trip.nil?
+      redirect_to trips_path, notice: 'Trip not found'
+      return
+    end
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      flash[:success] = "Trip added successfully"
+      redirect_to trip_path(@trip.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    id = params[:id]
+    @trip = Trip.find_by(id: id)
+
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    id = params[:id]
+    @trip = Trip.find_by(id: id)
+
+    if @trip.nil?
+      head :not_found
+      return
+    elsif @trip.update(trip_params)
+      redirect_to trip_path(@trip.id)
+      flash[:success] = "Trip updated successfully"
+      return
+    else
+      render :edit
+      return
+    end
+  end
+  
+  def destroy
+    id = params[:id]
+    @trip = Trip.find_by(id: id)
+    if @Trip.nil?
+      head :not_found
+      return
+    end
+
+    # what should happen after we delete a trip??
+    # go back to previous page??
+    @trip.destroy
+    redirect_to drivers_path
+    flash[:success] = "Trip removed successfully"
+    return
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:cost, :rating, :date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :drivers 
   resources :passengers
+  resources :trips, except: [:index]
   
   get '/homepages', to: 'homepages#index', as: 'homepages'
-
   patch '/drivers/:id/toggle_available', to: 'drivers#toggle_available', as: 'toggle_available'
 
   root to: 'homepages#index'
