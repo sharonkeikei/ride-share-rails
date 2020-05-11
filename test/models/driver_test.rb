c385c95a5b1aea31a0b04317deee8ef78f69bbda
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-11 13:16:11 -0700

    Added tests in driver model to test for average rating method.

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -60,8 +60,45 @@ describe Driver do
 
   # Tests for methods you create should go here
   describe "custom methods" do
+      
+      before do
+        # Make a driver with trips
+        new_driver = Driver.new(name: "Kari", vin: "123", available: true)
+        new_driver.save
+        new_passenger = Passenger.create(name: "Kari", phone_num: "111-111-1211")
+        trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 1234)
+        trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 6334)
+        
+        # Make driver without trips
+        no_trips_driver = Driver.new(name: "Henry", vin: "456", available: true)
+        no_trips_driver.save
+      end
+
     describe "average rating" do
-      # Your code here
+
+      it "returns nil if driver has not driven any trips" do
+        expect(no_trips_driver.trips.length).must_equal 0
+        expect(no_trips_driver.average_rating).must_be_nil
+      end
+
+      it "accesses all the driver's trips for a driver with trips already driven" do
+        expect(new_driver.trips.length).must_equal 2
+      end
+
+      it "correctly computes the average rating" do
+        expect(new_driver.average_rating).must_equal 4
+      end
+
+      it "correctly computes average when another trip is added" do
+        expect(new_driver.trips.length).must_equal 2
+
+        # Add a new trip
+        trip_3 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 2, cost: 5)
+
+        expect(new_driver.trips.length).must_equal 3
+        # round average rating to nearest integer
+        expect(new_driver.average_rating).must_equal 3
+      end
     end
 
     describe "total earnings" do
